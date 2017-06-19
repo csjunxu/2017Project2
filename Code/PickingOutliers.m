0dc6e44b0fbb27882abe87d7aca8a2cfac95b612
XuJun <nankaimathxujun@gmail.com>
2017-06-19 14:36:53 +0800

    add

--- Code/PickingOutliers.m
+++ Code/PickingOutliers.m
@@ -1,5 +1,5 @@
 clear;
-Original_image_dir = 'C:\Users\csjunxu\Desktop\TIP2017\RID_Dataset\DatasetTIP\Canon_80D_ISO800\';
+Original_image_dir = 'C:\Users\csjunxu\Desktop\TIP2017\RID_Dataset\DatasetTIP\Canon_80D_ISO800mean\';
 fpath = fullfile(Original_image_dir, '*.JPG');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
@@ -15,12 +15,13 @@ for i = 1:im_num
     [h, w, ch] = size(ImY); % the height and width of each image
     GridH = 1:floor(h/NumPoints):h; % record the position of points in height
     GridW = 1:floor(w/NumPoints):w;% record the position of points in width
-    CropImY = imY(GridH, GridW, ch); % extract the points, and form the cropped smaller image
+    CropImY = ImY(GridH, GridW, ch); % extract the points, and form the cropped smaller image
     MeanIntensity(i) = mean(mean(CropImY)); % compute the mean intensity
-    ImName{i} = im_dir(i).name; % record the name of this image
+    ImName{i, 1} = im_dir(i).name; % record the name of this image
 end
 %% sorting the images accordingto their mean intensities
-[Index, SortedMeanIntensity] = sort(MeanIntensity, 'descending'); 
-ImName = ImName{Index};
-fprint(['The Highest and Lowest Mean Intensities are' SortedMeanIntensity(1) ' and ' SortedMeanIntensity(end)]);
-fprint(['The Corresponding Images are ' ImName{1} ' and ' ImName{end}]);
+[SortedMeanIntensity, Index] = sort(MeanIntensity, 'ascend'); 
+fprintf(['The Highest and Lowest Mean Intensities are ' num2str(SortedMeanIntensity(1)) ' and ' num2str(SortedMeanIntensity(end)) '.\n']);
+fprintf(['The Corresponding Images are ' ImName{Index(1)} ' and ' ImName{Index(end)} '.\n']);
+
+
