93a1e7ff3eb1ed7f77ca1ade5039a1dd1bb14529
XuJun <nankaimathxujun@gmail.com>
2017-06-19 14:30:21 +0800

    addd

--- Code/PickingOutliers.m
+++ Code/PickingOutliers.m
@@ -4,22 +4,23 @@ fpath = fullfile(Original_image_dir, '*.JPG');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 
-MeanIntensity = zeros(im_num, 1);
-ImName = cell(im_num, 1);
-NumPoints = 100;
+
+MeanIntensity = zeros(im_num, 1); % record mean intensity of each image
+ImName = cell(im_num, 1); % record the name of each image
+NumPoints = 100; % the number of points extracted from each dimension of each image
 for i = 1:im_num
-    Im = imread(fullfile(Original_image_dir, im_dir(i).name) );
-    ImYCbCr = rgb2ycbcr(Im);
-    ImY = ImYCbCr(:, :, 1);
-    [h, w, ch] = size(ImY);
-    GridH = 1:floor(h/NumPoints):h;
-    GridW = 1:floor(w/NumPoints):w;
-    CropImY = imY(GridH, GridW, ch);
-    MeanIntensity(i) = mean(mean(CropImY));
-    ImName{i} = im_dir(i).name;
+    Im = imread(fullfile(Original_image_dir, im_dir(i).name) ); % read image 
+    ImYCbCr = rgb2ycbcr(Im); % transform from RGB to YCbCr space
+    ImY = ImYCbCr(:, :, 1); % only use the Y channel, illumination information
+    [h, w, ch] = size(ImY); % the height and width of each image
+    GridH = 1:floor(h/NumPoints):h; % record the position of points in height
+    GridW = 1:floor(w/NumPoints):w;% record the position of points in width
+    CropImY = imY(GridH, GridW, ch); % extract the points, and form the cropped smaller image
+    MeanIntensity(i) = mean(mean(CropImY)); % compute the mean intensity
+    ImName{i} = im_dir(i).name; % record the name of this image
 end
 %% sorting the images accordingto their mean intensities
-[Index, SortedMeanIntensity] = sort(MeanIntensity, 'descending');
+[Index, SortedMeanIntensity] = sort(MeanIntensity, 'descending'); 
 ImName = ImName{Index};
 fprint(['The Highest and Lowest Mean Intensities are' SortedMeanIntensity(1) ' and ' SortedMeanIntensity(end)]);
 fprint(['The Corresponding Images are ' ImName{1} ' and ' ImName{end}]);
