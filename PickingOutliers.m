0a12385d3c19c97219ebb09c716f09d96810cd28
XuJun <nankaimathxujun@gmail.com>
2017-09-20 10:02:19 +0800

    ad

--- PickingOutliers.m
+++ PickingOutliers.m
@@ -9,13 +9,16 @@ MeanIntensity = zeros(im_num, 1); % record mean intensity of each image
 ImName = cell(im_num, 1); % record the name of each image
 NumPoints = 500; % the number of points extracted from each dimension of each image
 for i = 1:im_num
+    fprintf(['Processing the image: ' im_dir(i).name '.\n']);
     Im = imread(fullfile(Original_image_dir, im_dir(i).name) ); % read image 
     ImYCbCr = rgb2ycbcr(Im); % transform from RGB to YCbCr space
+    clear Im;
     ImY = ImYCbCr(:, :, 1); % only use the Y channel, illumination information
     [h, w, ch] = size(ImY); % the height and width of each image
     GridH = 1:floor(h/NumPoints):h; % record the position of points in height
     GridW = 1:floor(w/NumPoints):w;% record the position of points in width
     CropImY = ImY(GridH, GridW, ch); % extract the points, and form the cropped smaller image
+    clear ImY;
     MeanIntensity(i) = mean(mean(CropImY)); % compute the mean intensity
     ImName{i, 1} = im_dir(i).name; % record the name of this image
 end
